7abd520546be27924c46e605f5bbe77de3b2b143
Kevin Cazelles <kevin.cazelles@insileco.io>
2023-09-28 11:42:05 -0400

    Correctly hide and show card

--- static/js/filter_works.js
+++ static/js/filter_works.js
@@ -1,13 +1,38 @@
-function filterCards(checkboxElem) {
-    elms = document.getElementsByClassName('card_' + checkboxElem.name);
-    if (checkboxElem.checked) {
-        for (let i = 0; i < elms.length; i++) {
-            elms[i].style.display = 'inline';
+var project_tags = [];
+document.getElementById("projects_checkboxes")
+    .querySelectorAll("input")
+    .forEach(elm => project_tags.push(elm.name));
+
+function filterProjectCards(slc) {
+    elms = document.getElementsByClassName('project_card');
+
+    for (let i = 0; i < elms.length; i++) {
+        all_cls = elms[i].className.split(' ');
+        k = 0;
+        for (let j = 0; j < slc.length; j++) {
+            cls = 'project_card_' + slc[j]
+            if (all_cls.includes(cls)) {
+                k++;
+            }
         }
-    } else {
-        for (let i = 0; i < elms.length; i++) {
-            elms[i].style.display = 'none';
+        if (k) {
+            elms[i].parentElement.style.display = 'inline';
+        } else {
+            elms[i].parentElement.style.display = 'none';
         }
     }
 }
 
+function getProjectTagsChecked() {
+    var selected_project_tags = [];
+    elms = document.getElementById("projects_checkboxes")
+        .querySelectorAll("input");
+    for (let i = 0; i < elms.length; i++) {
+        if (elms[i].checked) {
+            selected_project_tags.push(elms[i].name)
+        }
+    }
+    return selected_project_tags;
+}
+
+filterProjectCards(getProjectTagsChecked());
